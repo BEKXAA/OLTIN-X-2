488479913a7004651c1f049735146166d65f7b6a
BEKXAA <behruzboho1255@gmail.com>
2025-06-18 12:30:58 +0500

    first commit

--- script.js
+++ script.js
@@ -43,7 +43,8 @@ function updateProfile() {
 // Admindan coin yuborish
 function sendCoins() {
   const admin = currentUser;
-  if (admin !== "Behruz") return alert("Faqat admin kirishi mumkin!");
+  if (admin !== "Behruz" ) return alert("Faqat admin kirishi mumkin!");
+  if (admin !== "Мага" ) return alert("Faqat admin kirishi mumkin!");
 
   const nick = document.getElementById("adminNick").value.trim();
   const amount = parseInt(document.getElementById("adminAmount").value);
